259991310c2780c35ed6dc2d0245c279ac5052ec
Jimmy <odylic.lin@gmail.com>
2021-08-09 10:42:24 -0700

    has basic working

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "../App.scss";
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
+        Test
+      </div>
+    );
+  }
+}
+
+export default App;
